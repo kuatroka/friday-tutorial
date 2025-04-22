f40030d0991b93c7a05f2ff6e5809f7e6cd4fca6
Sal Hernandez <sal@sesolabor.com>
2025-04-22 11:58:45 -0400

    Add MoonIcon and guestbook entry for clickclickonsal with unique greeting

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,12 +3,14 @@ import StarIcon from './StarIcon';
 import SmileyIcon from './SmileyIcon';
 import CompassIcon from './CompassIcon';
 import RobotIcon from './RobotIcon';
+import MoonIcon from './MoonIcon';
 
 const iconComponents: Record<string, React.FC<any>> = {
   StarIcon,
   SmileyIcon,
   CompassIcon,
-  RobotIcon
+  RobotIcon,
+  MoonIcon
 };
 
 export default iconComponents;
